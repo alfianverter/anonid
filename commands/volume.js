5998f7d8d96af64980fc45e1ef68a6bc427e0a74
alfianverter <38290702+alfianverter@users.noreply.github.com>
2018-07-22 03:11:54 +0700

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -24,7 +24,7 @@ exports.run = async(music, message, args, color, queue) => {
         
         let vEmbed = new Discord.RichEmbed()
         .setDescription(`I set the volume to: **${args1[1]}**`)
-        .setColor(color);
+        .setColor('#ff8f2f');
         
         message.channel.send(vEmbed);
         
